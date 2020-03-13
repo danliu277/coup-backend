807c84f4cf660bd9b1329f0547790611983f8750
flatironschool <danliu277@gmail.com>
2020-03-13 10:08:32 -0400

    Destroy user_game if user leaves room

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -1,11 +1,14 @@
 class RoomsChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    room = Room.find(params[:room])
+    room = Room.find(params[:room][:id])
     stream_for room
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    # Remove if user leaves room
+    user_game = UserGame.find_by(user_id: params[:room][:user], room_id: params[:room][:id])
+    user_game.destroy
   end
 end
