2d500e937aec56bbf3ee3815b8fc882df6380aa7
flatironschool <danliu277@gmail.com>
2020-03-16 16:49:54 -0400

    Add get single userGame route

--- app/controllers/user_games_controller.rb
+++ app/controllers/user_games_controller.rb
@@ -1,6 +1,11 @@
 class UserGamesController < ApplicationController
-    def get_user_games
+    def get_room_users
         user_games = Room.find(params[:id]).user_games
         render json: user_games
+    end
+
+    def get_user_game
+        user_game = UserGame.find(params[:id])
+        render json: user_game
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   get 'users/', to: 'users#index'
   post 'users/', to: 'users#create'
 
-  get 'user_games/:id', to: 'user_games#get_user_games'
+  # To check users in game
+  get 'user_games/room/:id', to: 'user_games#get_room_users'
+  get 'user_games/game/:id', to: 'user_games#get_user_game'
 
   get 'games/:id', to: 'games#get_game'
   patch 'games/:id', to: 'games#start_game'
