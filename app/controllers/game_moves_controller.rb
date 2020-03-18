193c326dad1ee55f3c263104a75e5af836bf4396
flatironschool <danliu277@gmail.com>
2020-03-18 10:07:08 -0400

    Add action to remove card

--- app/controllers/game_moves_controller.rb
+++ app/controllers/game_moves_controller.rb
@@ -9,26 +9,35 @@ class GameMovesController < ApplicationController
 
     private def handle_move(action, game, user_game, target_game)
         case action
+        # Income gain 1 coin
         when 0
             user_game.money += 1
             user_game.save
+        # Foreign aid gain 2 coins
         when 1
             user_game.money += 2
             user_game.save
+        # Coup lose 7 coins, target loses a card
         when 2
             user_game.money -= 7
+            target_game.user_cards.sample.destroy
             user_game.save
+        # Duke gain 3 coins
         when 3
             user_game.money += 3
             user_game.save
+        # Assassin lose 3 coins, target lose card
         when 4
             user_game.money -=3
+            target_game.user_cards.sample.destroy
             user_game.save
+        # Captain steal 3 coins from target
         when 5
             user_game.money += 2
             target_game.money -= 2
             user_game.save
             target_game.save
+        # Ambassador can change 2 cards
         when 6
         end
     end
